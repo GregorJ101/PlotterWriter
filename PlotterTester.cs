0b616536de453f3c84310df62f60fd7efd3090c4
Greg Engelstad <grege@pobox.com>
2016-08-25 15:15:47 -0700

    Add files via upload
    PlotterWriter Readme
    
    This solution creates a DLL that exposes a CLI interface which wraps code written to communicate with legacy devices attached via USB-to-serial and USB-to-parallel cables.  It has been developed for communicating with a Houston Instruments Image Maker graphic pen plotter, compatible with the better-known HP7475A plotter.  The code is fully functional and supports each HPGL command and instruction that the Image Maker supports, including code to read a series of digitized points and create a series of HPGL instructions to plot those same coordinates.
    
    The DLL is written in both native C++ and C++/CLI.  The native C++ makes all the Win32 calls and does all the work of communicating with the plotter, while the C++/CLI code wraps the native C++ code and exposes a CLI interface that can be called by .net languages.  In this case the test app is written in C#.
    
    The project PlotterDriver contains all the C++ code used in the solution.  It does all the work of locatin

---
+++ PlotterTester.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+using WrapperBaseClass;
+using ParallelPortWriter;
+using SerialPortWriter;
+using PlotterBuffer;
+using PlotterTester;
+
+namespace PlotterWritterDLLTester
+{
+    class CPlotterTesterApp
+    {
+        const bool SERIAL      = true;
+        const bool PARALLEL    = false;
+
+        static void Main (string[] args)
+        {
+            CPlotterTester pt = new CPlotterTester ();
+            pt.TestPlotter (SERIAL, CPlotterTester.TEST_PROGRAMS_2);
+        }
+    }
+}
